edcf0557fc38cbd629f419cbef2f6c37c78550ed
MiNeoSatory <stanislav.limanovski@gmail.com>
2022-09-24 09:58:23 +0300

    Изменен Procfile

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ __dirname = path.resolve();
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/client/build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
