99184e86e0152e9ef558b2799933a57629319364
MiNeoSatory <stanislav.limanovski@gmail.com>
2022-09-24 10:19:56 +0300

    Отключен cors для production

--- app.js
+++ app.js
@@ -27,11 +27,18 @@ const selectUser = (userId) => {
   return onlineUsers.find((user) => user.userId === userId);
 };
 
-const io = new Server(httpServer, {
-  cors: {
-    origin: `http://localhost:3000`,
-  },
-});
+/* let io;
+if (process.env.NODE_ENV === 'production') {
+  io = new Server(httpServer);
+} else {
+  io = new Server(httpServer, {
+    cors: {
+      origin: process.env.CLIENT_URL,
+    },
+  });
+} */
+
+const io = new Server(httpServer);
 
 io.on('connection', (socket) => {
   socket.on('addUser', (userId) => {
