e73dc15d40b11234ff00633a5531801958ddc0f4
MiNeoSatory <stanislav.limanovski@gmail.com>
2024-07-15 14:33:04 +0300

    cors

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ io.on('connection', (socket) => {
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors());
+process.env.NODE_END !== 'production' && app.use(cors());
 
 app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/article', require('./routes/article.routes'));
